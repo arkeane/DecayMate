8f8fe6e4f826d6708bc1dda955e88bfea3aad3f7
Ludovico Pestarino <ludovico@pestarino.xyz>
2025-11-25 19:29:00 +0100

    Initial Commit

---
+++ DecayMate/DecayMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecayMateApp.swift
+//  DecayMate
+//
+//  Created by Ludovico Pestarino on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DecayMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
